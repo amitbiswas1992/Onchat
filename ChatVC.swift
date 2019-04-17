a24131d95217bdc198198a9f4c1eec574f8d554f
Amit Biswas <biswas1992@hotmail.com>
2019-04-17 01:56:36 -0400

    Added toogle button

--- ChatVC.swift
+++ ChatVC.swift
@@ -10,21 +10,20 @@ import UIKit
 
 class ChatVC: UIViewController {
 
+   // Outlets
+    
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
