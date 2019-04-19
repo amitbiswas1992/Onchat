3ca8ceab36e3c51f50c8d50f28efde78dfac40b5
Amit Biswas <biswas1992@hotmail.com>
2019-04-19 03:29:47 -0400

    Create account and user register successfully done

--- CreateAccountVC.swift
+++ CreateAccountVC.swift
@@ -10,6 +10,38 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+   
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    
+    @IBOutlet weak var userImg: UIImageView!
+   
+    @IBAction func pickedAvatar(_ sender: Any) {
+    }
+    
+    @IBAction func pickBackGroundColor(_ sender: Any) {
+    }
+    @IBAction func createAccountPressed(_ sender: Any) {
+        guard let email = emailTxt.text, emailTxt.text != "" else{
+            return
+            
+        }
+        guard let pass = passwordTxt.text, passwordTxt.text != "" else {
+            return
+     }
+        AuthService.instance.resisterUser(email: email, password: pass){
+            (success) in
+            if success {
+               print("registered user!")
+                
+            }
+        }
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
